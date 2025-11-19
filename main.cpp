0e691dd94489c22f0a9e9339d1b389c981eb8cd6
Christian Jimenez <jimenezchristian429@gmail.com>
2025-11-18 20:47:22 -0800

    final notes for lab

--- main.cpp
+++ main.cpp
@@ -1,90 +1,109 @@
-
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 using namespace std;
 using namespace sf;
+
 
 
 class RainbowScreen : public Drawable
 {
 public:
 	RainbowScreen(int pixelWidth, int pixelHeight);
-	void draw(RenderTarget& target, RenderStates states) const override; // window.draw(..)
+	void draw(RenderTarget& target, RenderStates states) const override;  //window.draw(...) calls this polymorphically
 	void update();
 private:
-	VertexArray v;
-	Vector2f pixelSize;
+	VertexArray m_va; //array of pixel locations and their colors (draw pixels on the screen)
+	Vector2i m_pixel_size;  //size of screen 
 };
+
+void RainbowScreen::update()
+{
+	int pixelHeight = m_pixel_size.y;
+	int pixelWidth = m_pixel_size.x;
+
+	for (int i = 0; i < pixelHeight; i++)
+	{
+		//pick a random color for each row and draw it across the entire row
+		//going to kind of look like a rainbow thingy sorta
+		Uint8 r, g, b; //unsigned char
+		r = rand() % 256;
+		g = rand() % 256;
+		b = rand() % 256;
+		for (int j = 0; j < pixelWidth; j++)
+		{
+			m_va[i * pixelWidth + j].position = { (float)j, (float)i };
+			m_va[i * pixelWidth + j].color = { r,g,b };
+		}
+
+	}
+}
 
 RainbowScreen::RainbowScreen(int pixelWidth, int pixelHeight)
 {
-	pixelSize = { pixelWidth, pixelHeight };
-	v.setPrimitiveType(Points);
-	v.resize(pixelWidth * pixelHeight);
+	m_pixel_size = { pixelWidth, pixelHeight };
+	//populate the vertex array (all the pixels to draw)
+	m_va.setPrimitiveType(Points); //draw every point in the container
+	m_va.resize(pixelWidth * pixelHeight);
 }
 void RainbowScreen::draw(RenderTarget& target, RenderStates states) const
 {
-	target.draw(v);
+	target.draw(m_va);
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 int main()
 {
 	int pixelWidth = VideoMode::getDesktopMode().width;
 	int pixelHeight = VideoMode::getDesktopMode().height;
-
 	VideoMode vm(pixelWidth, pixelHeight);
-	RenderWindow window(vm, "Rainbow screen", Style::Default);
+	RenderWindow window(vm, "Rainbow Screen", Style::Default);
 
-	//Each loop is one frame
+	RainbowScreen screen(pixelWidth, pixelHeight);
+	bool update = true; //only update initially or when they left click
+
+	//Each iteration of this loop is 1 frame
 	while (window.isOpen())
 	{
-		//input
+		///Input
 		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == Event::Closed) window.close();
-			if (event.type == Event::MouseButtonPressed)
+			if (event.type == sf::Event::Closed) window.close(); //x button on top left was pressed
+			if (event.type == sf::Event::MouseButtonPressed)
 			{
-				if (event.mouseButton.button == Mouse::Left)
+				if (event.mouseButton.button == sf::Mouse::Left)
 				{
-					cout << "Left mouse button was pressed\n";
+					update = true;
+					cout << "Left mouse button was pressed" << endl;
 					cout << "Mouse x: " << event.mouseButton.x << endl;
 					cout << "Mouse y: " << event.mouseButton.y << endl;
 				}
 			}
 		}
+
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
 		}
 
-		//update
-		float radius = 150.0;
-		RectangleShape rectangle;
-		rectangle.setSize(Vector2f(100, 50));
-		CircleShape circle(150);
-		circle.setFillColor(Color::Magenta);
-		rectangle.setFillColor(Color::Yellow);
-		circle.setPosition(pixelWidth / 2, pixelHeight / 2);
-		circle.setOrigin(radius, radius);
-		rectangle.setPosition(pixelWidth / 2, pixelHeight / 2);
-		
-		//draw
-		window.clear();
-		window.draw(circle);
-		window.draw(rectangle);
-		window.display();
+		///Update
+
+		if (update)
+		{
+			screen.update();
+			update = false;
+		}
+
+		//float radius = 150.0;
+		//CircleShape circle(150);
+		//circle.setFillColor(Color::Magenta);
+		//circle.setPosition(960, 540);
+		//circle.setOrigin(radius, radius);
+
+		///Draw
+		window.clear();  //clear previous frame
+		//window.draw(stuff);
+		///window.draw(circle);
+		window.draw(screen);
+		window.display(); //draw all the things we told it to draw
 	}
 }
